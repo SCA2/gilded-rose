137c94348dab2e196406b661ec44ad46b2d793ec
SCA2 <tpryan_1965@yahoo.com>
2017-01-18 23:33:29 -0800

    refactor

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,60 +1,88 @@
 require_relative 'item.rb'
 
+class Product < Item
+
+  attr_reader :strategy
+  attr_reader :immortal
+
+  def initialize(name, sell_in, quality, strategy: strategy, immortal: false)
+    @strategy = strategy
+    @immortal = immortal
+    super(name, sell_in, quality)
+  end
+
+  def self.adjust_normal
+    Proc.new do |p|
+      if p.sell_in <= 0
+        p.adjust_quality_by(-2)
+      else
+        p.adjust_quality_by(-1)
+      end
+    end
+  end
+
+  def self.adjust_brie
+    Proc.new do |p|
+      if p.sell_in <= 0
+        p.adjust_quality_by(2)
+      else
+        p.adjust_quality_by(1)
+      end
+    end
+  end
+
+  def self.adjust_backstage
+    Proc.new do |p|
+      if p.sell_in <= 0
+        p.adjust_quality_by(-p.quality)
+      elsif p.sell_in < 6
+        p.adjust_quality_by(3)
+      elsif p.sell_in < 11
+        p.adjust_quality_by(2)
+      else
+        p.adjust_quality_by(1)
+      end
+    end
+  end
+
+  def self.adjust_conjured
+    Proc.new do |p|
+      p.adjust_quality_by(-2)
+    end
+  end
+
+  def adjust_quality
+    strategy.call(self) unless immortal
+  end
+
+  def adjust_sell_in
+    self.sell_in -= 1 unless immortal
+  end
+
+  def adjust_quality_by(n)
+    self.quality += n
+    self.quality = [self.quality, 50].min
+    self.quality = [self.quality, 0].max
+  end
+end
+
 class GildedRose
   @items = []
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    @items << Product.new("+5 Dexterity Vest", 10, 20, strategy: Product.adjust_normal)
+    @items << Product.new("Aged Brie", 2, 0, strategy: Product.adjust_brie)
+    @items << Product.new("Elixir of the Mongoose", 5, 7, strategy: Product.adjust_normal)
+    @items << Product.new("Sulfuras, Hand of Ragnaros", 0, 80, strategy: Product.adjust_normal, immortal: true)
+    @items << Product.new("Backstage passes to a TAFKAL80ETC concert", 15, 20, strategy: Product.adjust_backstage)
+    @items << Product.new("Conjured Mana Cake", 3, 6, strategy: Product.adjust_conjured)
   end
 
   def update_quality
-    @items.each do |product|
-      adjust_quality(product)
+    @items.each do |item|
+      item.adjust_quality
+      item.adjust_sell_in
     end
   end
-
-  def adjust_quality(product)
-    if product.name == "Sulfuras, Hand of Ragnaros"
-      return
-    elsif product.name == "Aged Brie"
-      if product.sell_in <= 0
-        increase_quality(product, 2)
-      else
-        increase_quality(product, 1)
-      end
-    elsif product.name == "Backstage passes to a TAFKAL80ETC concert"
-      if product.sell_in <= 0
-        product.quality = 0
-      elsif (product.sell_in < 6)
-        increase_quality(product, 3)
-      elsif (product.sell_in < 11)
-        increase_quality(product, 2)
-      else
-        increase_quality(product, 1)
-      end
-    else
-      if product.sell_in <= 0
-        decrease_quality(product, 2)
-      else
-        decrease_quality(product, 1)
-      end
-    end
-
-    product.sell_in -= 1
-  end
-
-  def increase_quality(product, n)
-    product.quality = [product.quality + n, 50].min
-  end
-
-  def decrease_quality(product, n)
-    product.quality = [product.quality - n, 0].max
-  end
-
 end
